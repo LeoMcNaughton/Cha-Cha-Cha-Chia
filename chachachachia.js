8b730f68ca490ad0b1a81e5d39275b124d1d2f21
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-11-16 14:41:47 -0600

    Add files via upload

---
+++ chachachachia.js
@@ -0,0 +1,14 @@
+const require = ('./tree.js');
+const require = ('./queue.js');
+const require = ('./random.js');
+
+function chachachia(forestSize){
+    let forestSize = ;
+    let newQueue = ;
+    for(i=0;i<forestsize.length;i++){
+        plantSeeds.enqueue(random.discreteRangeIn(1,1000000))
+    }
+    let forest = new Tree(0);
+
+    for
+}
